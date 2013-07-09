3854bdc9da26909ca5cdcc2e18d2f48dd63ca9d2
Ubuntu <ubuntu@ip-10-124-174-159.ec2.internal>
2013-07-09 19:36:59 +0000

    Temp

--- grader.js
+++ grader.js
@@ -65,8 +65,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+.option('-u,  --url <url>', 'Url to html file')
         .parse(process.argv);
+if(program.url){                
+var fs = require('fs');
+var rest = require('restler');
+        var url = program.url.toString();              
+        rest.get(url).on('complete', function(result, response){   
+var urlFile = "hello.txt";
+fs.writeFileSync(urlFile,result);
+ var checkJson = checkHtmlFile(urlFile, program.checks);
+});}
+else{
     var checkJson = checkHtmlFile(program.file, program.checks);
+}
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
